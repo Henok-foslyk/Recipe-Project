003420239a300e401130d93a15efbf8c0945ded8
Okailey <quaye22d@mtholyoke.edu>
2025-06-02 15:47:21 -0400

    navbar and recipe page

--- recipe-project/src/routes/Recipes.jsx
+++ recipe-project/src/routes/Recipes.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+import "../styles/recipe.css";
+import Navbar from "../components/Navbar";
+import Skeleton from 'react-loading-skeleton';
+import { useEffect, useState } from "react";
+
 
 export default function Recipes() {
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+
   return (
-    <div>This is the recipes page</div>
+    <>
+      <Navbar />
+
+      <div className="recipe-container">
+        This is the recipes page
+        <div class="recipe-grid">
+          <div class="skeleton-card"></div>
+          <div class="skeleton-card"></div>
+          <div class="skeleton-card"></div>
+          <div class="skeleton-card"></div>
+          <div class="skeleton-card"></div>
+          <div class="skeleton-card"></div>
+          <div class="skeleton-card"></div>
+          <div class="skeleton-card"></div>
+        </div>
+        <Skeleton height={20} width={200} />
+      </div>
+    </>
   )
 }
