51123aec8dd6eae7c45a950deccb9b00bbc1df7c
Henok-foslyk <henokmisginafisseha@gmail.com>
2025-06-04 11:47:21 -0400

    rudimentary home page ... needs styling

--- recipe-project/src/App.jsx
+++ recipe-project/src/App.jsx
@@ -2,6 +2,8 @@ import { React } from 'react'
 import Navbar from "./components/Navbar";
 import Home from "./routes/Home";
 import './App.css'
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function App() {
 
